b65d0b0d81293c7fe53607bd499357e8b4d10c46
PSLmoein <AFKmoein@proton.me>
2025-01-29 08:03:56 -0800

    restframework serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 import json
 
+from products.models import Product
+from products.serializers import ProductSerializer
 
-def api_home(request, *args, **kwarg):
-    print(request.GET) #url query params
-    body= request.body
-    print(body)
-    data= {}
-    try:
-        data = json.loads(body) #String of JSON data -> python dict
-    except:
-        pass
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    print(data)
+@api_view(['GET'])
+def api_home(request, *args, **kwarg): 
+    instance = Product.objects.all().order_by("?").first()
     
-    return JsonResponse(data)
+    data = ProductSerializer(instance).data
+                       
+    return Response(data)
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,4 +5,5 @@ endpoint = "http://127.0.0.1:8000/api/"
 response = requests.get(endpoint, params={'qwe': 132}, json={'query':'hello world'})
 
 print(response.status_code)
+print(response.json())
 
